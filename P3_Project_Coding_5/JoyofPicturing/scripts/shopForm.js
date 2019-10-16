5dd8f3916331af00faf72548dc4a18f06d8ad33e
TormodNy <tormod-n@hotmail.com>
2019-10-16 13:35:30 +0200

    Javascript og andre endringer

--- P3_Project_Coding_5/JoyofPicturing/scripts/shopForm.js
+++ P3_Project_Coding_5/JoyofPicturing/scripts/shopForm.js
@@ -3,5 +3,12 @@ const formEl = document.getElementById("shopcontent");
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    text = "";
+    for(i = 0; i < formEl.length - 1; i++){
+        text += "<p>" + formEl.elements[i].name[0].toUpperCase() + formEl.elements[i].name.substring(1) + ": " + formEl.elements[i].value + " </p>";
+    }
+
     formEl.style.display = "none";
+
+    bodyEl.innerHTML += "<div><h2>Order details</h2><p>Your order has been placed successfully.</p> " + text + "</div>";
 });
