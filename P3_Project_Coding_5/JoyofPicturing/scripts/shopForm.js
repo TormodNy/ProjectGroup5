55a7484e6d7b40dc826c6a668f8d5f52907ca74e
TormodNy <tormod-n@hotmail.com>
2019-10-17 11:46:03 +0200

    Ferdig med Shopform V1

--- P3_Project_Coding_5/JoyofPicturing/scripts/shopForm.js
+++ P3_Project_Coding_5/JoyofPicturing/scripts/shopForm.js
@@ -5,10 +5,13 @@ formEl.addEventListener("submit", (e) => {
 
     text = "";
     for(i = 0; i < formEl.length - 1; i++){
-        text += "<p>" + formEl.elements[i].name[0].toUpperCase() + formEl.elements[i].name.substring(1) + ": " + formEl.elements[i].value + " </p>";
+        text += "<tr>";
+        text += "<td>" + formEl.elements[i].name[0].toUpperCase() + formEl.elements[i].name.substring(1) + ": </td>";
+        text += "<td>" + formEl.elements[i].value + " </td>";
+        text += "</td>";
     }
 
     formEl.style.display = "none";
 
-    bodyEl.innerHTML += "<div><h2>Order details</h2><p>Your order has been placed successfully.</p> " + text + "</div>";
+    bodyEl.innerHTML += "<div><h2>Order details</h2><p>Your order has been placed successfully.</p> <table>" + text + "</table></div>";
 });
