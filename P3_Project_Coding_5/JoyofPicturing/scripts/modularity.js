f38a7b9332b163bc5e1e6d226b90b29599b80aa9
Henrik Moe Wæhre <henrikmw@live.no>
2019-10-23 14:09:32 +0200

    added gallery.js, gallery.html and gallery.css. Made some minor changes to style.css

--- P3_Project_Coding_5/JoyofPicturing/scripts/modularity.js
+++ P3_Project_Coding_5/JoyofPicturing/scripts/modularity.js
@@ -1,11 +1,14 @@
 let bodyEl = document.querySelector('body');
 
+// moved the a element outside the p element so that the link applies to the whole p-element,
+// not the text
+
 bodyEl.innerHTML += '<header id="navbar">'
                     + '<p id="joy">Joy of Picturing</p>'
-                    + '<p id="about" class="navbutton"><a href="about.html">About & Contact</a></p>'
-                    + '<p id="prizes" class="navbutton"><a href="prices.html">Prices</a></p>'
-                    + '<p id="gallery" class="navbutton"><a href="gallery.html">Gallery</a></p>'
-                    + '<p id="home" class="navbutton"><a href="index.html">Home</a></p>'
+                    + '<a href="about.html"><p id="about" class="navbutton">ABOUT & CONTACT</p></a>'
+                    + '<a href="prices.html"><p id="prices" class="navbutton">PRICES</p></a>'
+                    + '<a href="gallery.html"><p id="gallery" class="navbutton">GALLERY</p></a>'
+                    + '<a href="index.html"><p id="home" class="navbutton">HOME</p></a>'
                     + '</header>'
                     + '<footer>'
                     + '<p>&#9400; Copyright 2019. All rights reserved.</p>'
