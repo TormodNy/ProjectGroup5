c71404995575e17720147d5b8006262938adb7f8
TormodNy <tormod-n@hotmail.com>
2019-10-31 12:37:31 +0100

    Tormod

--- P3_Project_Coding_5/JoyofPicturing/scripts/gallery.js
+++ P3_Project_Coding_5/JoyofPicturing/scripts/gallery.js
@@ -4,7 +4,12 @@ const galleryEl = document.querySelector(".gallery")
 for (var i=1;i<imgNr;i++) {
     console.log(i);
     // galleryEl.innerHTML+="<div id='imgContainer"+i+"' class='galleryImgContainer'>";
-    galleryEl.innerHTML+="<div class='imgWrapper'><img src='img/resized/"+i+".jpg' id='img"+i+"' class='galleryImg'></div>";
+    // galleryEl.innerHTML += "<div class='imgWrapper'><img src='img/resized/"+i+".jpg' id='img"+i+"' class='galleryImg'></div>";
     // galleryEl.innerHTML+="</div>";
-    
+    const newDiv = makeElement("div", "", "", "imgWrapper");
+    const newImg = makeElement("img", "", "img" + i, "galleryImg")
+    newImg.src = "img/resized/" + i + ".jpg";
+    newImg.alt = "Thumbnail " + i;
+    newDiv.appendChild(newImg);
+    galleryEl.appendChild(newDiv);
 }
