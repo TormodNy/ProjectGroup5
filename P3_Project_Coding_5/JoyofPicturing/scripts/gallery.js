568b4528798562d03613c6245597f2d8fa5cc0e0
Henrik Moe Wæhre <henrikmw@live.no>
2019-10-31 12:35:11 +0100

    changed images to be dynamically added in javascript. Fixed some minor css issues

--- P3_Project_Coding_5/JoyofPicturing/scripts/gallery.js
+++ P3_Project_Coding_5/JoyofPicturing/scripts/gallery.js
@@ -2,5 +2,23 @@ const imgNr=13; //indicates how many images are to be added
 const galleryEl = document.querySelector(".gallery")
 
 for (var i=1;i<imgNr;i++) {
-    galleryEl.innerHTML+="<div class='imgWrapper'><img src='img/resized/"+i+".jpg' id='img"+i+"' class='galleryImg'></div>";
+    imgWrapperEl=document.createElement("div");
+    imgWrapperEl.classList.add("imgWrapper");
+    galleryEl.appendChild(imgWrapperEl);
+    
+    imgEl=document.createElement("img");
+    imgEl.src="img/resized/"+i+".jpg";
+    imgEl.classList.add("galleryImg");
+    imgEl.id="img"+i;
+    imgWrapperEl.appendChild(imgEl);
+    
+    
+    // galleryEl.innerHTML+="<div class='imgWrapper'><img src='img/resized/"+i+".jpg' id='img"+i+"' class='galleryImg'></div>";
+}
+
+galleryEl.addEventListener("click", previewFunction);
+
+
+function previewFunction() {
+    
 }
